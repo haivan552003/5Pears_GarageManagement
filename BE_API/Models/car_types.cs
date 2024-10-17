ac2c6c54f47fc12a384a7295963e0d216e16b857
letanbao1209 <baoltpc07211@fpt.edu.vn>
2024-10-17 23:28:23 +0700

    aa

---
+++ BE_API/Models/car_types.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BE_API.Models
+{
+    public class car_types
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime date_create { get; set; }
+        public DateTime date_update { get; set; }
+    }
+}
---
+++ FE_Admin/Models/car_types.cs
@@ -0,0 +1,10 @@
+﻿namespace FE_Admin.Models
+{
+    public class car_types
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime date_create { get; set; }
+        public DateTime date_update { get; set; }
+    }
+}
