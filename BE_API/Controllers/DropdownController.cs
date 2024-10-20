d5793329b5842b2d919b7d548c8324823a1e8690
VanLTH <haivan5.5.2003@gmail.com>
2024-10-21 00:03:54 +0700

    api dropdown

--- BE_API/Controllers/DropdownController.cs
+++ BE_API/Controllers/DropdownController.cs
@@ -32,5 +32,100 @@ namespace BE_API.Controllers
             }
 
         }
+        [HttpGet("customer")]
+        public async Task<ActionResult<IEnumerable<dropdown>>> DropdownCustomer()
+        {
+            var procedureName = "sp_dropdown_customer";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var dropdown = await connection.QueryAsync<dropdown>(procedureName, commandType: CommandType.StoredProcedure);
+                return Ok(dropdown);
+            }
+
+        }
+        [HttpGet("driver")]
+        public async Task<ActionResult<IEnumerable<dropdown>>> DropdownDriver()
+        {
+            var procedureName = "sp_dropdown_driver";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var dropdown = await connection.QueryAsync<dropdown>(procedureName, commandType: CommandType.StoredProcedure);
+                return Ok(dropdown);
+            }
+
+        }
+        [HttpGet("location")]
+        public async Task<ActionResult<IEnumerable<dropdown>>> DropdownLocation()
+        {
+            var procedureName = "sp_dropdown_location";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var dropdown = await connection.QueryAsync<dropdown>(procedureName, commandType: CommandType.StoredProcedure);
+                return Ok(dropdown);
+            }
+
+        }
+        [HttpGet("car")]
+        public async Task<ActionResult<IEnumerable<dropdown>>> DropdownCar()
+        {
+            var procedureName = "sp_dropdown_car";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var dropdown = await connection.QueryAsync<dropdown>(procedureName, commandType: CommandType.StoredProcedure);
+                return Ok(dropdown);
+            }
+
+        }
+        [HttpGet("trip")]
+        public async Task<ActionResult<IEnumerable<dropdown>>> DropdownTrip()
+        {
+            var procedureName = "sp_dropdown_trip";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var dropdown = await connection.QueryAsync<dropdown>(procedureName, commandType: CommandType.StoredProcedure);
+                return Ok(dropdown);
+            }
+
+        }
+        [HttpGet("tripdetail")]
+        public async Task<ActionResult<IEnumerable<dropdown>>> DropdownTripDetail()
+        {
+            var procedureName = "sp_dropdown_tripdetail";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var dropdown = await connection.QueryAsync<dropdown>(procedureName, commandType: CommandType.StoredProcedure);
+                return Ok(dropdown);
+            }
+
+        }
+
+        [HttpGet("carseat")]
+        public async Task<ActionResult<IEnumerable<dropdown>>> DropdownCarseat(int car_id)
+        {
+            var procedureName = "sp_dropdown_carseat";
+            var parameters = new DynamicParameters();
+            parameters.Add("car_id", car_id, DbType.Int32, ParameterDirection.Input);
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                var product = await connection.QueryFirstOrDefaultAsync<dropdown>(
+                    procedureName, parameters, commandType: CommandType.StoredProcedure);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(product);
+            }
+
+        }
     }
 }
