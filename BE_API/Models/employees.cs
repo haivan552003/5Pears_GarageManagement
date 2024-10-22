4adce647dfa417d0fabdd988a0121e35a6387bfe
VanLTH <haivan5.5.2003@gmail.com>
2024-10-22 19:41:30 +0700

    fix emp

--- BE_API/Models/employees.cs
+++ BE_API/Models/employees.cs
@@ -10,16 +10,18 @@ namespace BE_API.Models
         public string email { get; set; }
         public string emp_code { get; set; }
         public bool status { get; set; }
-        
+        public string img_emp { get; set; }
         public string password { get; set; }
         public string fullname { get; set; }
+        public string role_name { get; set; }
+        public string phone_number { get; set; }
         public DateTime birthday { get; set; }
+        public DateTime date_create { get; set; }
         public string citizen_identity_img { get; set; }
         public string citizen_identity_number { get; set; }
         public bool gender { get; set; }
 
         public int role_id { get; set; }
-        public string role_name { get; set; }
     }
 
     public class employees_create
--- FE_Admin/Models/employees.cs
+++ FE_Admin/Models/employees.cs
@@ -6,15 +6,16 @@
         public string email { get; set; }
         public string emp_code { get; set; }
         public bool status { get; set; }
-
+        public string img_emp { get; set; }
         public string password { get; set; }
         public string fullname { get; set; }
-        public DateTime? birthday { get; set; }
+        public string role_name { get; set; }
+        public string phone_number { get; set; }
+        public DateTime birthday { get; set; }
         public string citizen_identity_img { get; set; }
         public string citizen_identity_number { get; set; }
         public bool gender { get; set; }
-
+        public DateTime date_create { get; set; }
         public int role_id { get; set; }
-        public string role_name { get; set; }
     }
 }
