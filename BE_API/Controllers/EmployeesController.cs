b1ca96ffe6f252b768cefe94e086d08a77526f85
VanLTH <haivan5.5.2003@gmail.com>
2024-10-22 10:15:46 +0700

    fix admin emp

--- BE_API/Controllers/EmployeesController.cs
+++ BE_API/Controllers/EmployeesController.cs
@@ -35,7 +35,6 @@ namespace BE_API.Controllers
 
 
         // GET: api/Products/5
-
         [HttpGet("{id}")]
         public async Task<ActionResult<employees>> sp_view_EmployeesID(int id)
         {
@@ -60,67 +59,49 @@ namespace BE_API.Controllers
         }
         // POST: api/employees
         [HttpPost]
-        public async Task<ActionResult> AddEmployees([FromBody] employees newEmployee)
+        public async Task<ActionResult> AddEmployees(employees_create newEmployee)
         {
-            // Kiểm tra xem đối tượng có hợp lệ không
-            if (newEmployee == null || string.IsNullOrEmpty(newEmployee.email) || string.IsNullOrEmpty(newEmployee.password))
+            try
             {
-                return BadRequest("Dữ liệu không hợp lệ.");
+                var hashedPassword = BCrypt.Net.BCrypt.HashPassword(newEmployee.password);
+                var parameters = new DynamicParameters(newEmployee);
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    var result = await connection.ExecuteAsync("sp_add_employee", parameters, commandType: CommandType.StoredProcedure);
+
+                    if (result > 0)
+                    {
+                        return Ok();
+                    }
+                    else
+                    {
+                        return BadRequest();
+                    }
+                }
             }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Lỗi: {ex.Message}");
+            }
+        }
 
-            // Mã hóa mật khẩu
-            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(newEmployee.password);
 
-            // Sử dụng DynamicParameters để truyền tham số
-            var parameters = new DynamicParameters();
-            parameters.Add("@user_name", newEmployee.email, DbType.String);
-            parameters.Add("@emp_code", newEmployee.emp_code, DbType.String);
-            parameters.Add("@pass_word", hashedPassword, DbType.String);
-            parameters.Add("@full_name", newEmployee.fullname, DbType.String);
-            parameters.Add("@birthday", newEmployee.birthday, DbType.Date);
-            parameters.Add("@citizen_identity_img", newEmployee.citizen_identity_img, DbType.String);
-            parameters.Add("@citizen_identity_number", newEmployee.citizen_identity_number, DbType.String);
-            parameters.Add("@status", newEmployee.status, DbType.String);
-            parameters.Add("@gender", newEmployee.gender, DbType.String);
-            parameters.Add("@id_role", newEmployee.role_id, DbType.Int32);
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployees(int id, employees_update employee)
+        {
+            var parameters = new DynamicParameters(employee);
+            parameters.Add("@id", id);
 
             using (var connection = new SqlConnection(_connectionString))
             {
                 await connection.OpenAsync();
-                var result = await connection.ExecuteAsync("sp_add_employee", parameters, commandType: CommandType.StoredProcedure);
 
-                if (result > 0)
-                {
-                    return Ok(new { message = "Nhân viên đã được thêm thành công" });
-                }
+                var result = await connection.ExecuteAsync("sp_updateemployee", parameters, commandType: CommandType.StoredProcedure);
 
-                return BadRequest(new { message = "Thêm nhân viên không thành công" });
-            }
-        }
-
-        // PUT: api/Products/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutEmployees(int id, employees employees)
-        {
-            using (SqlConnection conn = new SqlConnection(_connectionString))
-            {
-                SqlCommand cmd = new SqlCommand("sp_updateemployee", conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                cmd.Parameters.AddWithValue("@id_emp", id); 
-                cmd.Parameters.AddWithValue("@full_name", employees.fullname);
-                cmd.Parameters.AddWithValue("@birthday", employees.birthday);
-                cmd.Parameters.AddWithValue("@citizen_identity_img", employees.citizen_identity_img);
-                cmd.Parameters.AddWithValue("@citizen_identity_number", employees.citizen_identity_number);
-                cmd.Parameters.AddWithValue("@status", employees.status); 
-                cmd.Parameters.AddWithValue("@gender", employees.gender);
-                cmd.Parameters.AddWithValue("@id_role", employees.role_id);
-
-                await conn.OpenAsync();
-
-                int rowsAffected = await cmd.ExecuteNonQueryAsync();
-
-                if (rowsAffected == 0)
+                if (result == 0)
                 {
                     return NotFound();
                 }
@@ -130,12 +111,13 @@ namespace BE_API.Controllers
         }
 
 
+
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteEmployee(int id)
         {
             try
             {
-                using (var connection = new SqlConnection(_connectionString)) 
+                using (var connection = new SqlConnection(_connectionString))
                 {
                     await connection.OpenAsync();
 
--- BE_API/Models/employees.cs
+++ BE_API/Models/employees.cs
@@ -22,4 +22,28 @@ namespace BE_API.Models
         public string role_name { get; set; }
     }
 
+    public class employees_create
+    {
+        public string email { get; set; }
+        public string emp_code { get; set; }
+        public bool status { get; set; }
+        public string password { get; set; }
+        public string fullname { get; set; }
+        public DateTime birthday { get; set; }
+        public string citizen_identity_img { get; set; }
+        public string citizen_identity_number { get; set; }
+        public bool gender { get; set; }
+        public int role_id { get; set; }
+    }
+    public class employees_update
+    {
+        public string password { get; set; }
+        public string fullname { get; set; }
+        public DateTime birthday { get; set; }
+        public string citizen_identity_img { get; set; }
+        public string citizen_identity_number { get; set; }
+        public bool gender { get; set; }
+        public int role_id { get; set; }
+        public bool status { get; set; }
+    }
 }
