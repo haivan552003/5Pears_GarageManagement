8bb7731c8faeb1fe97c63d42e5c57b99b1d7bd06
minhHieu2424 <kingbobbb024@gmail.com>
2024-10-22 10:37:02 +0700

    [fix ckedit firebase form news]

---
+++ FE_Admin/wwwroot/ckeditor-interop.js
@@ -0,0 +1,7 @@
+﻿window.initCKEditor = (elementId, dotnetRef) => {
+    CKEDITOR.replace(elementId);
+    CKEDITOR.instances[elementId].on('change', function () {
+        let data = CKEDITOR.instances[elementId].getData();
+        dotnetRef.invokeMethodAsync('OnChange', data);
+    });
+};
