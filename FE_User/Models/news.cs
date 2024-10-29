8a557b609d4c03590b9c4afcfc01b8b4782420e7
minhHieu2424 <kingbobbb024@gmail.com>
2024-10-29 20:54:35 +0700

    [FE_admin new,new detail]

--- FE_User/Models/news.cs
+++ FE_User/Models/news.cs
@@ -4,13 +4,15 @@ namespace FE_User.Models
 {
     public class news
     {
-        public int id_news { get; set; }
+        public int id { get; set; }
         public string news_img { get; set; }
         public string title { get; set; }
         public string content { get; set; }
         public DateTime date_create { get; set; }
         public DateTime date_update { get; set; }
         public int id_emp { get; set; }
+        public Boolean status { get; set; }
+        public string fullname { get; set; }
 
     }
 }
