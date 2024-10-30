d97e6a90dc050555fffc9845bbfdd3aeb6b75a4a
VanLTH <haivan5.5.2003@gmail.com>
2024-10-30 21:49:34 +0700

    homepage

--- FE_User/ModelCustom/login.cs
+++ FE_User/ModelCustom/login.cs
@@ -2,7 +2,7 @@
 {
     public class login
     {
-        public string username { get; set; }
+        public string email { get; set; }
         public string password { get; set; }
         public int role_id { get; set; }
     }
