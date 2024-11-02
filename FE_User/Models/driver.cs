d2f2fe7d21b46a6a5e2dd03500d1707f7613f721
VanLTH <haivan5.5.2003@gmail.com>
2024-11-02 16:52:26 +0700

    get driver

--- FE_User/Models/driver.cs
+++ FE_User/Models/driver.cs
@@ -5,18 +5,19 @@ namespace FE_User.Models
 {
     public class driver
     {
-        public int id_driver { get; set; }
+        public int id { get; set; }
         public string img_driver { get; set; }
-        public string full_name { get; set; }
+        public string fullname { get; set; }
         public DateTime birthday { get; set; }
         public string citizen_identity_img { get; set; }
         public string citizen_identity_number { get; set; }
         public byte gender { get; set; }
         public string driver_license_img { get; set; }
         public string driver_license_number { get; set; }
-        public string status { get; set; }
+        public byte status { get; set; }
+        public string phonenumber { get; set; }
         public byte is_delete { get; set; }
-        public DateTime date_create { get; set; }
-        public DateTime date_update { get; set; }
+        public int age { get; set; }
+        public float price { get; set; }
     }
 }
