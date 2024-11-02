5ad4b97d0df3f4cfc56456658beb18c162bef751
quiLV <quilevan1234abc@gmail.com>
2024-11-02 14:51:33 +0700

    GetDetail_ChiTietThueXe

--- FE_User/Models/car.cs
+++ FE_User/Models/car.cs
@@ -6,6 +6,7 @@ namespace FE_User.Models
     public class car
     {
         public int id { get; set; }
+        public string car_name { get; set; }
         public string img_name { get; set; }
 
         public string car_number { get; set; }
@@ -24,6 +25,12 @@ namespace FE_User.Models
         public DateTime year_production { get; set; }
         public float odo { get; set; }
         public float insurance_fee { get; set; }
+
+        public int fuel {  get; set; }
+        public string description {  get; set; }
+        public float voucher {  get; set; }
+        public List<car_img> car_img { get; set; }
+
 
     }
 }
