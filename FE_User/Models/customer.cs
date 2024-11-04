a3a6a2d1d157506937f0531f798ff155af58a21a
minhHieu2424 <kingbobbb024@gmail.com>
2024-11-04 23:03:12 +0700

    [đổi mật khẩu, profile user]

--- FE_User/Models/customer.cs
+++ FE_User/Models/customer.cs
@@ -5,19 +5,23 @@ namespace FE_User.Models
 {
     public class customer
     {
-        public int id_cus { get; set; }
-
-        public string user_name { get; set; }
-        public string pass_word { get; set; }
-        public string full_name { get; set; }
+        public int Id { get; set; }
+        public string cus_code { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string fullname { get; set; }
         public DateTime birthday { get; set; }
         public byte gender { get; set; }
         public string phone_number { get; set; }
-        public string citizen_identity_img { get; set; }
+        public string citizen_identity_img1 { get; set; }
+        public string citizen_identity_img2 { get; set; }
         public string citizen_identity_number { get; set; }
-        public string driver_license_img { get; set; }
+        public string driver_license_img1 { get; set; }
+        public string driver_license_img2 { get; set; }
         public string driver_license_number { get; set; }
-        public byte is_delete { get; set; }
         public int id_role { get; set; }
+        public string name { get; set; }
+        public bool status { get; set; }
+        public string img_cus { get; set; }
     }
 }
--- FE_User/Models/customer_address.cs
+++ FE_User/Models/customer_address.cs
@@ -4,12 +4,10 @@ namespace FE_User.Models
 {
     public class customer_address
     {
-        public int id_address { get; set; }
+        public int id { get; set; }
         public string address { get; set; }
-        public string type { get; set; }
-        public byte is_delete { get; set; }
-        public DateTime date_create { get; set; }
-        public DateTime date_update { get; set; }
         public int id_cus { get; set; }
+        public byte type { get; set; }
+        public bool status { get; set; }
     }
 }
