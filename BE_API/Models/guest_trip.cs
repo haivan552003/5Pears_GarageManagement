baba3bafe1780390f94248a13d1b5bee90c03331
VanLTH <haivan5.5.2003@gmail.com>
2024-11-12 19:54:23 +0700

    (..)

--- BE_API/Models/guest_trip.cs
+++ BE_API/Models/guest_trip.cs
@@ -22,6 +22,7 @@ namespace BE_API.Models
         public string location_to { get; set; }
         public DateTime time_start { get; set; }
         public DateTime time_end { get; set; }
+        public DateTime pay_date { get; set; }
         public float price { get; set; }
         public float return_money { get; set; }
         public int status { get; set; }
