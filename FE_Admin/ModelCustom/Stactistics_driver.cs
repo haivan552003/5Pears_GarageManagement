ab3bdb6b759cd44eee2bf0e7c26cdb06398a8e70
minhHieu2424 <kingbobbb024@gmail.com>
2024-11-16 22:39:38 +0700

    biểu đồ tài xế

---
+++ FE_Admin/ModelCustom/Stactistics_driver.cs
@@ -0,0 +1,39 @@
+﻿namespace FE_Admin.ModelCustom
+{
+    public class Stactistics_driver
+    {
+    }
+    public class MonthlyStatistic
+    {
+        public int Month { get; set; }
+        public int Total { get; set; }
+        public decimal Revenue { get; set; }
+    }
+    public class statistics_driver_by_GCD
+    {
+        public int id { get; set; }
+        public string FullName { get; set; }
+        public string Driver_code { get; set; }
+    }
+    public class statistics_driver_by_GCD_monly
+    {
+        public int Month { get; set; }
+        public int Total { get; set; }
+
+        public decimal Revenue { get; set; }
+    }
+
+
+    public class statistics_driver_by_trip
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int TotalTrips { get; set; }
+        public string Driver_code { get; set; }
+    }
+    public class statistics_driver_by_trip_monly
+    {
+        public int Month { get; set; }
+        public int Total { get; set; }
+    }
+}
