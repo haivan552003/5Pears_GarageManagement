ed22e068db2223b58a36074a99d01f1be0b975b0
VanLTH <haivan5.5.2003@gmail.com>
2024-11-16 21:35:31 +0700

    payment guest trip

---
+++ FE_User/ModelCustom/PaymentRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FE_User.ModelCustom
+{
+    public class PaymentRequestModel
+    {
+        public string OrderId { get; set; }
+        public decimal Amount { get; set; }
+        public string OrderInfo { get; set; }
+
+    }
+}
