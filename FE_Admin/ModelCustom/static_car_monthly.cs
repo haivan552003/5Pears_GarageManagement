95292d856d2efac1daf0d44ac17b662c3873cee9
letanbao1209 <baoltpc07211@fpt.edu.vn>
2024-11-18 01:22:10 +0700

    ThongKeThueXe2

---
+++ FE_Admin/ModelCustom/static_car_monthly.cs
@@ -0,0 +1,9 @@
+﻿namespace FE_Admin.ModelCustom
+{
+    public class static_car_monthly
+    {
+        public int Month { get; set; }
+        public int TripCount { get; set; }
+        public decimal TotalRevenue { get; set; }
+    }
+}
