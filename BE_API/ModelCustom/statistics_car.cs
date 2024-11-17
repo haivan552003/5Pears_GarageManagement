9d3e00efe94108f0fb653ff6021276332d929e31
letanbao1209 <baoltpc07211@fpt.edu.vn>
2024-11-17 19:42:10 +0700

    ThongKeThueXe1

---
+++ BE_API/ModelCustom/statistics_car.cs
@@ -0,0 +1,11 @@
+﻿namespace BE_API.ModelCustom
+{
+    public class statistics_car
+    {
+        public int id { get; set; }
+        public string car_code { get; set; }
+        public string car_name { get; set; }
+        public string car_number { get; set; }
+
+    }
+}
---
+++ BE_API/ModelCustom/statistics_car_monthly.cs
@@ -0,0 +1,10 @@
+﻿namespace BE_API.ModelCustom
+{
+    public class statistics_car_monthly
+    {
+        public int Month { get; set; }
+        public int TripCount { get; set; }
+        public decimal TotalRevenue { get; set; }
+
+    }
+}
