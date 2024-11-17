e1220ee6af973f6af025b121fa11b80b365f03cb
letanbao1209 <baoltpc07211@fpt.edu.vn>
2024-11-17 13:03:41 +0700

    ThongKe

---
+++ BE_API/Controllers/StatisticsGuestCarController.cs
@@ -0,0 +1,50 @@
+﻿using BE_API.ModelCustom;
+using BE_API.Models;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace BE_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsGuestCarController : ControllerBase
+    {
+        private readonly string _connectionString;
+        public StatisticsGuestCarController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("SqlConnection");
+        }
+
+        [HttpGet("Car_Month_Statistics")]
+        public async Task<ActionResult<CarMonthStatistics>> GetCarMonthStatisticsAsync([FromQuery] int carId, [FromQuery] int month, [FromQuery] int year)
+        {
+            if (carId <= 0 || month < 1 || month > 12 || year <= 0)
+                return BadRequest("Invalid parameters.");
+
+            using var connection = new SqlConnection(_connectionString);
+            var parameters = new DynamicParameters();
+            parameters.Add("@CarId", carId);
+            parameters.Add("@Month", month);
+            parameters.Add("@Year", year);
+
+            var result = await connection.QueryFirstOrDefaultAsync<CarMonthStatistics>(
+                "sp_statistics_car_month1",
+                parameters,
+                commandType: CommandType.StoredProcedure
+            );
+
+            if (result == null)
+                return NotFound("No data found for the specified parameters.");
+
+            return Ok(result);
+        }
+
+    }
+}
---
+++ BE_API/Models/CarMonthStatistics.cs
@@ -0,0 +1,7 @@
+﻿namespace BE_API.Models
+{
+    public class CarMonthStatistics
+    {
+        public int TotalTrips { get; set; }
+    }
+}
