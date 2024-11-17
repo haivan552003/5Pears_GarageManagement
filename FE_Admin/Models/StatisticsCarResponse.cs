e5659e283d03e0cc1586fc39e01fbd27007ec1ab
letanbao1209 <baoltpc07211@fpt.edu.vn>
2024-11-17 15:20:13 +0700

    s

--- FE_Admin/Models/StatisticsCarResponse.cs
+++ FE_Admin/Models/StatisticsCarResponse.cs
@@ -3,11 +3,13 @@
     public class StatisticsCarResponse
     {
         public int id { get; set; }
-        public int Month { get; set; }
         public string CarCode { get; set; }
         public string CarName { get; set; }
+    }
+    public class MonthlyStatistic
+    {
+        public int Month { get; set; }
         public int TripCount { get; set; }
         public decimal TotalRevenue { get; set; }
-        public string GuestCarCodes { get; set; }
     }
 }
