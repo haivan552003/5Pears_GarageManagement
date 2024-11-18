caa74a28517358741ce5d2b6b251c1a6a2339947
hoangthinh1512 <thinhlhpc07113@fpt.edu.vn>
2024-11-18 15:18:22 +0700

    daxong.

---
+++ BE_API/ModelCustom/DetailedTripStatistics.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BE_API.ModelCustom
+{
+    public class DetailedTripStatistics
+    {
+        public int TripID { get; set; }
+        public string TripCode { get; set; }
+        public string LocationFrom { get; set; }
+        public string LocationTo { get; set; }
+        public int TicketsSold { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+    }
+}
---
+++ BE_API/ModelCustom/MonthlyTripStatistics.cs
@@ -0,0 +1,10 @@
+﻿namespace BE_API.ModelCustom
+{
+    public class MonthlyTripStatistics
+    {
+
+        public int Month { get; set; }
+        public int TicketsSold { get; set; }
+        public decimal Revenue { get; set; }
+    }
+}
