cfbf65b04a0a2dcb04fad73bbf1150db75831058
minhHieu2424 <kingbobbb024@gmail.com>
2024-12-07 16:21:29 +0700

    lịch tài xế

---
+++ BE_API/ModelCustom/calendarDriver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BE_API.ModelCustom
+{
+    public class calendarDriver
+    {
+        public int id { get; set; }
+        public string fullname { get; set; }
+        public DateTime date { get; set; }
+        public DateTime? date_start { get; set; }
+        public DateTime? date_end { get; set; }
+    }
+    public class DriverDropdown
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string img { get; set; }
+    }
+}
--- FE_Admin/ModelCustom/dropdown.cs
+++ FE_Admin/ModelCustom/dropdown.cs
@@ -6,4 +6,19 @@
         public string name { get; set; }
         public string img { get; set; }
     }
+    public class DriverDropdown
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string img { get; set; }
+    }
+
+    public class DriverCalendar
+    {
+        public int id { get; set; }
+        public string fullname { get; set; }
+        public DateTime date { get; set; }
+        public DateTime? date_start { get; set; }
+        public DateTime? date_end { get; set; }
+    }
 }
