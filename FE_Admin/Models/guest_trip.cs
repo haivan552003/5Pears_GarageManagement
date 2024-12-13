9618cf355d730d2afcd921216039a3074d3b7499
quiLV <quilevan1234abc@gmail.com>
2024-12-13 23:02:36 +0700

    qui_Checkout

--- FE_Admin/Models/guest_trip.cs
+++ FE_Admin/Models/guest_trip.cs
@@ -40,6 +40,46 @@
         public DateTime time_start { get; set; }
         public DateTime time_end { get; set; }
     }
+    public class guest_trip_checkout
+    {
+        public int id { get; set; }
+        public int emp_id { get; set; }
+        public int cus_id { get; set; }
+        public int trip_id { get; set; }
+        public int trip_detail_id { get; set; }
+        public int location_from_id { get; set; }
+        public int location_to_id { get; set; }
+        public int car_id { get; set; }
+        public string guest_trip_code { get; set; }
+        public string trip_detail_code { get; set; }
+        public string emp_name { get; set; }
+        public string cus_name { get; set; }
+        public string location_from { get; set; }
+        public string location_to { get; set; }
+        public DateTime time_start { get; set; }
+        public DateTime time_end { get; set; }
+        public DateTime pay_date { get; set; }
+        public float price { get; set; }
+        public float return_money { get; set; }
+        public int status { get; set; }
+        public int car_seat_id { get; set; }
+        public bool payment_method { get; set; }
+        public DateTime date_create { get; set; }
+        public string email { get; set; }
+        public string phone_number { get; set; }
+        public string car_name { get; set; }
+        public string from { get; set; }
+        public string to { get; set; }
+        public List<guest_trip_child> guest_trip_child { get; set; }
+    }
+    public class guest_trip_update
+    {
+        public bool? payment_method { get; set; }
+        public string bank_code { get; set; }
+        public string card_type { get; set; }
+        public string? transaction_no { get; set; }
+        public string? transaction_status { get; set; }
+    }
     public class guest_trip_create
     {
         public int id { get; set; }
