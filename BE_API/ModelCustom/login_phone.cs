97dead6ccb5c9731c8363e8359ae4f56afb9f4a1
quiLV <quilevan1234abc@gmail.com>
2024-12-18 20:15:38 +0700

    LoginPhone

---
+++ BE_API/ModelCustom/login_phone.cs
@@ -0,0 +1,21 @@
+﻿namespace BE_API.ModelCustom
+{
+    public class login_phone_user
+    {
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        //public string phonenumber { get; set; }
+        //public string password { get; set; }
+        //public string fullname { get; set; }
+        //public int role_id { get; set; }
+        //public int id { get; set; }
+    }
+    public class LoginResponseDto
+    {
+        public string PhoneNumber { get; set; }
+        public string fullname { get; set; }
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+
+}
---
+++ FE_User/ModelCustom/login_phone.cs
@@ -0,0 +1,16 @@
+﻿namespace FE_User.ModelCustom
+{
+    public class login_phone
+    {
+        public string phone_number { get; set; }
+        public string password { get; set; }
+        public int role_id { get; set; }
+    }
+    public class LoginResponseDto
+    {
+        public string PhoneNumber { get; set; }
+        public string FullName { get; set; }
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+}
